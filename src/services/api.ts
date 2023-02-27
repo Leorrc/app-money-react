a0ffb0c217819c8affde8985b62523a931a353ac
Leonardo Carvalho <100796907+Leorrc@users.noreply.github.com>
2023-02-27 14:23:08 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+ // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://app-money-react.vercel.app/api',
 
 })
